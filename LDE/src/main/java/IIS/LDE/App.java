e327a5b8e1a84f26b0d4e18dad697827efd6dcf4
ivanJimenezM <2ivan.jimenez.moyano2@gmail.com>
2015-04-01 12:53:41 +0200

    Creo proyecto maven IIS.LDE

---
+++ LDE/src/main/java/IIS/LDE/App.java
@@ -0,0 +1,13 @@
+package IIS.LDE;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
